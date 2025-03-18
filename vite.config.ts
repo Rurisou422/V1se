b9b3a63d9f6a6f945f7026eb644faa76c634d249
Rurisou422 <rurisour@gmail.com>
2025-03-18 19:54:58 +0100

    Remove complex configuration and simplify build process

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    minify: 'esbuild',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
